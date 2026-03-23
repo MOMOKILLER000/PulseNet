2e4dadc0439d3a69dfd8381e786c96aa011416fc
MOMOKILLER000 <buzduganmihnea@gmail.com>
2026-03-23 17:11:13 +0200

    Improved admin page

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -6,6 +6,7 @@ from django.utils.crypto  import get_random_string
 from django.db.models import F
 # Create your models here.
 from django.utils import timezone
+from pgvector.django import VectorField
 
 class User(AbstractUser):
 
@@ -349,6 +350,8 @@ class Alert(models.Model):
         ("weather", "Weather Alert"),
         ("lost", "Lost Item"),
         ("found", "Found Item"),
+        ("lost_pet", "Lost Pet"),
+        ("found_pet", "Found Pet"),
         ("traffic", "Traffic Alert"),
         ("safety", "Safety Notice"),
         ("event", "Event"),
@@ -364,6 +367,8 @@ class Alert(models.Model):
 
     title = models.CharField(max_length=150)
     description = models.TextField()
+
+    embedding = VectorField(dimensions=512, null=True, blank=True)
 
     is_approved = models.BooleanField(default=True)
     is_flagged = models.BooleanField(default=False)
@@ -396,6 +401,7 @@ class AlertImage(models.Model):
         related_name="images"
     )
     image = models.ImageField(upload_to="alert_images/")
+    embedding = VectorField(dimensions=512, null=True, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 class AlertConfirm(models.Model):
@@ -487,6 +493,7 @@ class Notification(models.Model):
         ("rental_proposal", "Rental Proposal"),
         ("chat_message", "Chat Message"),
         ("hero_alert", "Hero Alert"),
+        ("pet_match", "Pet Match Found"),
     )
 
     user = models.ForeignKey(
